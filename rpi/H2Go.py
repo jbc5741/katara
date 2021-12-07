aac2a3f13372830e3ac20d7930f58837bc2f8c14
robxbob <rphernan@ucsd.edu>
2021-12-07 09:31:23 -0800

    AAdded water tracking

--- rpi/H2Go.py
+++ rpi/H2Go.py
@@ -32,31 +32,55 @@ def cleanAndExit():
     print("Bye!")
     sys.exit()
 
-def record_water_intake():
+def record_water_intake(water_intake):
     date = datetime.now()
-    date_str = strftime("%Y%m%d%H%M") 
-    return date_str 
+    date_str = date.strftime("%Y%m%d%H%M")
+    ref = db.reference('readings')
+    ref_get = ref.get()
+    x = [data for data in ref_get.each() if json.loads(data.val())['datetime'] == date_str]
+    y = 0
+    if len(x) > 0:
+        z = json.loads(x[0].val())
+        y = z['water']
+    water_total  = x + y 
+
+    final_data = {'datetime': date_str, 'water': water_total}
+    final_data = json.dumps(final_data)
+    if len(x) == 0:
+        ref.push(final_data)
+    else:
+        ref.child(x[0].key()).set(final_data)
 
 
-
-
+stable = False
+water_level = 0
 while True:
     try:
         # LOGIC TO SEE IF DRINKING OCCURRED
         weight = hx.get_weight(READ_NUM)
+        if weight < 0:
+            weight = 0
         dd_arr.append(weight)
-        
+
         if len(dd_arr) >= DD_MAX_LEN:
             dd_arr = dd_arr[1:]
-        if np.std(dd_arr) / np.average() <= DD_THRESHOLD:
-            record_water_intake()
+        avg = np.average(dd_arr)
+        if avg > 0 and np.std(dd_arr) / avg <= DD_THRESHOLD:
+            stable = True
+            # Stable water level, check if water level decreased
+            if avg < water_level:
+                water_level = avg
+                record_water_intake(avg)
+        else:
+            stable = False
+
         
         # data = { "datetime": datetime.now(), "weight": val }
         # data = json.dumps(data)
         # db.child("readings").child("weights").push(data)
-        adate = datetime.now()
-        x = adate.strftime("%Y%m%d%H%M") 
-        print(val + " " + x)
+        # adate = datetime.now()
+        # x = adate.strftime("%Y%m%d%H%M") 
+        # print(val + " " + x)
 
         hx.power_down()
         hx.power_up()
--- rpi/databasetester.py
+++ rpi/databasetester.py
@@ -14,11 +14,37 @@ fb = Firebase(config)
 db = fb.database()
 
 
-for i in range(10):
-    date = datetime.now()
-    new_minute = (date.minute + i ) % 60
-    record_date = datetime(date.year, date.month, date.day, date.hour, date.minute + i)
-    record_date_str = record_date.strftime("%Y%m%d%H%M") 
-    data = { "datetime": record_date_str, "water": random.randint(20, 120) }
-    data = json.dumps(data)
-    db.child("readings").push(data)
+# for i in range(10):
+#     date = datetime.now()
+#     new_minute = (date.minute + i ) % 60
+#     record_date = datetime(date.year, date.month, date.day, date.hour, date.minute + i)
+#     record_date_str = record_date.strftime("%Y%m%d%H%M") 
+#     data = { "datetime": record_date_str, "water": random.randint(20, 120) }
+#     data = json.dumps(data)
+#     db.child("readings").push(data)
+
+# for i in range(10):
+#     date = datetime.now()
+#     new_minute = (date.minute + i ) % 60
+#     record_date = datetime(date.year, date.month, date.day, date.hour, date.minute + i)
+#     record_date_str = record_date.strftime("%Y%m%d%H%M") 
+#     data = { "water": random.randint(20, 120) }
+#     data = json.dumps(data)
+#     db.child("readings").child(record_date_str).push(data)
+
+ref = db.child('readings').get()
+x = [data for data in ref.each() if json.loads(data.val())['datetime'] == '202112061537']
+y = x[0].key()
+print(type("ds"))
+print(y)
+z = {'datetime': '202112061538', 'water': 1111}
+z = json.dumps(z)
+print(z)
+db.child('readings').child(y).set(z)
+# print(ref)  
+
+# x[0].update({'water': 1111})
+
+
+# for data in ref.each():
+#     print(json.loads(data.val())['datetime'])
